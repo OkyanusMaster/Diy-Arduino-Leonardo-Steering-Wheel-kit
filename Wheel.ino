bad68fe2ae6355af6126a16b102263646ccdc6c0
OkyanusMaster <91731475+OkyanusMaster@users.noreply.github.com>
2022-11-22 23:48:44 +0300

    update geldi el freni ve ekstra butonlar eklendi

--- Wheel.ino
+++ Wheel.ino
@@ -11,24 +11,29 @@ void setup() {
   pinMode(4, INPUT_PULLUP);
   pinMode(5, INPUT_PULLUP);
   pinMode(6, INPUT_PULLUP);
+  pinMode(7, INPUT_PULLUP);
+  pinMode(8, INPUT_PULLUP);
+  pinMode(9, INPUT_PULLUP);
+
   //Set joystick axis range
   Joystick.begin();
   Joystick.setXAxisRange(0, 1023);
-  Joystick.setYAxisRange(360, 907);
-  Joystick.setZAxisRange(307, 807);
-  Joystick.setRxAxisRange(338, 825);
+  Joystick.setYAxisRange(0, 1023);
+  Joystick.setZAxisRange(0, 1023);
+  Joystick.setRxAxisRange(0, 1023);
+  Joystick.setRzAxisRange(0, 1023);
   //Define potantiometer
   pinMode(A0, INPUT);
   pinMode(A1, INPUT);
   pinMode(A2, INPUT);
+  pinMode(A3, INPUT);
   pinMode(A4, INPUT);
   Serial.begin(9600);
-
 }
 const int pinToButtonMap = 2;
 
 // Last state of the button
-int lastButtonState[5] = {0, 0, 0, 0, 0};
+int lastButtonState[5] = { 0, 0, 0, 0, 0 };
 void loop() {
   // Axes are assigned to the potentiometer
   int pot1 = analogRead(A0);
@@ -40,15 +45,15 @@ void loop() {
   int pot3 = analogRead(A2);
   Joystick.setRxAxis(pot3);
 
-  int pot4 = analogRead(A4);
+  int pot4 = analogRead(A3);
   Joystick.setZAxis(pot4);
-  Serial.println(pot4);
 
-  for (int index = 0; index < 5; index++)
-  {
+  int pot5 = analogRead(A4);
+  Joystick.setRzAxis(pot5);
+
+  for (int index = 0; index < 7; index++) {
     int currentButtonState = !digitalRead(index + pinToButtonMap);
-    if (currentButtonState != lastButtonState[index])
-    {
+    if (currentButtonState != lastButtonState[index]) {
       Joystick.setButton(index, currentButtonState);
       lastButtonState[index] = currentButtonState;
     }
